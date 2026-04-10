acdc986b23ab2f23c0dba2af7ca914e3bda6d38e
njofredev <nico.jofreandrade@gmail.com>
2026-04-10 12:03:16 -0400

    fix: resolve module resolution by adding .dockerignore and baseUrl in tsconfig.json

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'standalone',
-  // @ts-ignore
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // @ts-ignore
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
